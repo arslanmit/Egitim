0b896eb2a6b45d2df847bee7900a12d64e8d15a4
Umit Arslan <umit.arslan@outlook.com>
2016-10-09 02:33:10 +0300

    Sd

--- YouTube API/python/skyscanner-python-sdk-master/OpenNotify/script.py
+++ YouTube API/python/skyscanner-python-sdk-master/OpenNotify/script.py
@@ -4,7 +4,7 @@ import json
 
 # Make a get request to get the latest position of the international space station from the opennotify api.
 response = requests.get("http://api.open-notify.org/iss-now.json")
-print(response)
+
 
 status_code=response.status_code
 print(status_code)
@@ -29,14 +29,18 @@ print(response.content)
 
 # Make a list of fast food chains.
 best_food_chains = ["Taco Bell", "Shake Shack", "Chipotle"]
+print(best_food_chains)
 print(type(best_food_chains))
 
 
 # Use json.dumps to convert best_food_chains to a string.
 best_food_chains_string = json.dumps(best_food_chains)
+print(best_food_chains_string)
 print(type(best_food_chains_string))
 
+
 # Convert best_food_chains_string back into a list
+print(json.loads(best_food_chains_string))
 print(type(json.loads(best_food_chains_string)))
 
 # Make a dictionary
@@ -46,7 +50,33 @@ fast_food_franchise = {
     "Starbucks": 10821,
     "Pizza Hut": 7600
 }
+print(fast_food_franchise)
+print(type(fast_food_franchise))
 
 # We can also dump a dictionary to a string and load it.
 fast_food_franchise_string = json.dumps(fast_food_franchise)
+print(fast_food_franchise_string)
 print(type(fast_food_franchise_string))
+
+fast_food_franchise_2=json.loads(fast_food_franchise_string)
+print(fast_food_franchise_2)
+print(type(fast_food_franchise_2))
+
+parameters = {"lat": 37.78, "lon": -122.41}
+response = requests.get("http://api.open-notify.org/iss-pass.json", params=parameters)
+print(response)
+# Get the response data as a python object.  Verify that it's a dictionary.
+data = response.json()
+
+print(data)
+print(type(data))
+
+print(data["response"][0]["duration"])
+print(response.headers)
+
+
+
+response = requests.get("http://api.open-notify.org/astros.json")
+print(response.json())
+
+in_space_count=response.json()["number"]
